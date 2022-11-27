713aa3de7c148af5d67f6e79113429fd017a0d1d
Jesus <tjesus@lopes.com.br>
2022-11-27 13:48:12 -0300

    add log, refactor file car_analises

--- batch/car_analises.py
+++ batch/car_analises.py
@@ -1,17 +1,28 @@
 from pyspark.sql import SparkSession,functions as f
+from logger import Log4j
+
 
 if __name__ == '__main__':
 
-    spark = SparkSession.builder.config('spark.app.name','Cars Analises').enableHiveSupport().getOrCreate()
+    spark = SparkSession.builder.config('spark.app.name','XXX').enableHiveSupport().getOrCreate()
+    spark.sparkContext.setLogLevel('WARN')
+
+    log = Log4j(spark)
+
     cars_df = spark.read.option("multiline", "true").json(f'resources/cars.json')
     
+
+    log.warn('========================Show cars tabl========================S')
+
+
     cars_df.show()
 
-    #Count rows
+    log.info('========================SCount rows========================S')
     print(f'Total rows: ${cars_df.count()}')
     cars_df.groupBy('Origin').agg(f.count('Origin')).show()
 
-    #Save the result
+    
+    log.info('========================SSave the result========================S')
     cars_df.write.format('parquet').mode('overwrite').save('output/grouped_estates/')
 
 
